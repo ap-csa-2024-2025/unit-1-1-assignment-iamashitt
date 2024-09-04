755dd848054e5a7edf9863898fc4f43068e64cde
iamashitt <bribo60322@gmail.com>
2024-09-04 10:03:56 -0700

    wyuwyiyiw

--- problem2.java
+++ problem2.java
@@ -1,1 +1,10 @@
+public class HelloWorldln
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Hello world!");
+    System.out.println("Hello world!");
+    System.out.println("Hello world!");
+  }
+}
 
